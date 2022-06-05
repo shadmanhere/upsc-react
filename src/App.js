727c392830cb4cc1f7a661bfe37df5d265d02125
Shadman Ali <shadman.ali@live.co.uk>
2022-06-05 15:53:31 +0530

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Upsc App</h1>
-    </div>
+    <Navbar />
   );
 }
 
